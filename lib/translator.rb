3def2e827c692ea5ba4f89c52396e79b8771ed52
fearless-command-2358 <iuriseara@gmail.com>
2020-06-05 15:48:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,29 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(library)
+  # code goes
+  hash = {
+    "get_meaning" => {},
+    "get_emoticon" => {}
+  }
+  library.each do |meaning, translation|
+    english = translation[0]
+    japanese = translation[1]
+    hash["get_meaning"][japanese] = meaning
+    hash["get_emoticon"][english] = japanese
+end
+  return hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
+  library = load_library(file_path)
+  library["get_emoticon"].each do |key, value|
+    if key == emoticon
+      puts value
+    end
+  end
 end
 
 def get_english_meaning
